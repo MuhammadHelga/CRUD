c7b26bbd4256d98e94b03b6005df303f5ae9acb1
MuhammadHelga <128779298+MuhammadHelga@users.noreply.github.com>
2023-10-30 16:36:54 +0700

    Add files via upload

--- CRUD/apps/boot.php
+++ CRUD/apps/boot.php
@@ -1,4 +1,5 @@
 <?php
+require "config.php";
 require "controller.php";
 require "Database.php";
 require "model.php";
--- CRUD/apps/config.php
+++ CRUD/apps/config.php
@@ -4,4 +4,5 @@ define('DB_USER','root');
 define('DB_PASS','');
 define('DB_TABLE','mvcapp');
 define('DB_DRIVER','mysql');
+define('BASE_URL', 'http://localhost/CRUD/');
 ?>
